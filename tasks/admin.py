a7ec3d9a66c1600e358e50e18ac995e9d04f07c4
Omar <omardavidjm@ufps.edu.co>
2024-12-05 07:49:12 -0500

    feat: register Item in admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Item
 
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price')
+    
+admin.register(Item, ItemAdmin)
 # Register your models here.
